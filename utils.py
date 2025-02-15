1f97246821b57e21de3fc0775a4ded3378261041
Ruslana-Martsynko <martsynko_ruslana_@knu.ua>
2025-02-15 13:48:17 +0200

    Додано функцію is_power_of_five

--- utils.py
+++ utils.py
@@ -2,3 +2,10 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
+
+def is_power_of_five(n):
+    if n < 1:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
